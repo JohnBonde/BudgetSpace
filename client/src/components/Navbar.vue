bf2416a91a02ce2a5a545286ecd72a106577dd5c
JohnBonde <jjbonde1@gmail.com>
2020-03-04 15:26:13 -0700

    added navbar

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand">BudgetSpace</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div
+      class="collapse navbar-collapse"
+      id="navbarSupportedContent"
+      style="justify-content: flex-end"
+    >
+      <button @click="logout" class="btn btn-outline-danger my-2 my-sm-0" type="button">Logout</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+  methods: {
+    logout() {
+      this.$store.dispatch("logout");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,58 +1,54 @@
 <template>
-  <div class="home container-fluid">
-    <div class="row">
-      <div class="col">
-        <button
-          v-if="!myBudget"
-          type="button"
-          class="btn btn-primary"
-          @click="showModal"
-        >
-          Create Budget
-        </button>
-        <button
-          v-if="myBudget"
-          type="button"
-          class="btn btn-warning"
-          @click="
+  <div class="home">
+    <header class="container-fluid">
+      <navbar />
+    </header>
+    <div class="body container-fluid">
+      <div class="row">
+        <div class="col">
+          <button
+            v-if="!myBudget"
+            type="button"
+            class="btn btn-primary"
+            @click="showModal"
+          >Create Budget</button>
+          <button
+            v-if="myBudget"
+            type="button"
+            class="btn btn-warning"
+            @click="
             showModal2();
             forceRerender();
           "
-        >
-          Edit Budget
-        </button>
-        <button type="button" class="btn btn-danger" @click="logout">
-          Logout
-        </button>
+          >Edit Budget</button>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <budget
-          :budgetData="myBudget"
-          :userData="budgets"
-          :key="componentKey"
-        />
+      <div class="row">
+        <div class="col">
+          <budget :budgetData="myBudget" :userData="budgets" :key="componentKey" />
+        </div>
       </div>
+      <create-modal v-show="isModalVisible" @close="closeModal" />
+      <edit-modal
+        :bData="myBudget"
+        :uData="userInfo"
+        :key="componentKey"
+        v-show="isModalVisible2"
+        @close="closeModal2"
+      />
     </div>
-    <create-modal v-show="isModalVisible" @close="closeModal" />
-    <edit-modal
-      :bData="myBudget"
-      :uData="userInfo"
-      :key="componentKey"
-      v-show="isModalVisible2"
-      @close="closeModal2"
-    />
   </div>
 </template>
 
 <script>
+import Navbar from "../components/Navbar";
 import CreateModal from "../components/CreateModal";
 import EditModal from "../components/EditModal";
 import Budget from "../components/Budget";
 export default {
   name: "home",
   components: {
+    Navbar,
     CreateModal,
     EditModal,
     Budget
@@ -106,3 +102,8 @@ export default {
   }
 };
 </script>
+<style scoped>
+header {
+  padding: 0;
+}
+</style>
